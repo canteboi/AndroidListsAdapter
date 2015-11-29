d6cd12c22dbcc1d988f04f636464b6a0fe3b39c5
cante <flordecante.ibanez@hp.com>
2015-11-29 22:12:10 +0800

    final

---
+++ app/src/main/java/com/example/ibanezf/androidweatherapp/model/CurrentWeather.java
@@ -0,0 +1,127 @@
+package com.example.ibanezf.androidweatherapp.model;
+
+import com.example.ibanezf.androidweatherapp.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by ibanezf on 11/29/2015.
+ */
+public class CurrentWeather {
+    //region Properties
+    private String mIcon;
+    private long mtime;
+    private double mTemp;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+    private String mTimeZone;
+
+    public String getTimeZone() {
+        return mTimeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        mTimeZone = timeZone;
+    }
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public int getIconId(){
+        int iconId = R.mipmap.clear_day;
+
+        if (mIcon.equals("clear-day")) {
+            iconId = R.mipmap.clear_day;
+        }
+        else if (mIcon.equals("clear-night")) {
+            iconId = R.mipmap.clear_night;
+        }
+        else if (mIcon.equals("rain")) {
+            iconId = R.mipmap.rain;
+        }
+        else if (mIcon.equals("snow")) {
+            iconId = R.mipmap.snow;
+        }
+        else if (mIcon.equals("sleet")) {
+            iconId = R.mipmap.sleet;
+        }
+        else if (mIcon.equals("wind")) {
+            iconId = R.mipmap.wind;
+        }
+        else if (mIcon.equals("fog")) {
+            iconId = R.mipmap.fog;
+        }
+        else if (mIcon.equals("cloudy")) {
+            iconId = R.mipmap.cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-day")) {
+            iconId = R.mipmap.partly_cloudy;
+        }
+        else if (mIcon.equals("partly-cloudy-night")) {
+            iconId = R.mipmap.cloudy_night;
+        }
+
+        return iconId;
+    }
+
+    public long getMtime() {
+        return mtime;
+    }
+
+    public String getFormattedTime(){
+        SimpleDateFormat formatter = new SimpleDateFormat("h:mm a");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimeZone));
+        Date dateTime = new Date(getMtime() * 1000);
+        String timeString = formatter.format(dateTime);
+
+        return timeString;
+    }
+
+    public void setMtime(long mtime) {
+        this.mtime = mtime;
+    }
+
+    public int getTemp() {
+        return (int)Math.round(mTemp);
+    }
+
+    public void setTemp(double temp) {
+        mTemp = temp;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        double precipPercent = mPrecipChance* 100;
+        return (int)Math.round(precipPercent);
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+    //endregion
+
+
+}
--- app/src/main/java/com/example/ibanezf/androidweatherapp/ui/MainActivity.java
+++ app/src/main/java/com/example/ibanezf/androidweatherapp/ui/MainActivity.java
@@ -1,35 +1,51 @@
 package com.example.ibanezf.androidweatherapp.ui;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.ibanezf.androidweatherapp.R;
+import com.example.ibanezf.androidweatherapp.model.CurrentWeather;
 import com.squareup.okhttp.Call;
 import com.squareup.okhttp.Callback;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private final String apikey = "134806da256b7476535cc3ebfc240aad";
-    //private double latitude = 37.8267 ;
-    private double latitude = 9999 ;
-    private double longhitude = -122.423 ;
     private final OkHttpClient client = new OkHttpClient();
 
+    private CurrentWeather mCurrentWeather;
 
+    //private TextView mTemperatureLabel;
+    @Bind(R.id.timeLabel) TextView mTimeLabel;
+    @Bind(R.id.temperatureLabel) TextView mTemperatureLabel;
+    @Bind(R.id.humidityValue) TextView mHumidityValue;
+    @Bind(R.id.precipValue) TextView mPrecipValue;
+    @Bind(R.id.summaryLabel) TextView mSummaryLabel;
+    @Bind(R.id.iconImageView) ImageView mIconImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         setupApi();
     }
@@ -38,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
 
         if (isNetWorkAvailable()) {
             String url = getString(R.string.forecast_url);
+            String apikey = "134806da256b7476535cc3ebfc240aad";
+            double latitude = 14.6042000;
+            double longhitude = 120.9822000;
             String forecastUrl = String.format(url
                     , apikey
                     , latitude + ""
@@ -57,14 +76,20 @@ public class MainActivity extends AppCompatActivity {
                 public void onResponse(Response response) throws IOException {
                     try {
                         //Response response = call.execute();
-
                         if (!response.isSuccessful()) {
                             alertuserAboutError();
                         }
+                        String jsonData = response.body().string();
 
-                        Log.v(TAG, response.body().string());
+                        mCurrentWeather = getCurrentDetails(jsonData);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                updateDisplay();
+                            }
+                        });
 
-                    } catch (IOException e) {
+                    } catch (IOException | JSONException e) {
                         Log.e(TAG, "Exception caught: ", e);
                     }
                 }
@@ -78,8 +103,42 @@ public class MainActivity extends AppCompatActivity {
             dialog.setAlertTitle(title);
             dialog.setAlertMessage(message);
 
-            dialog.show(getFragmentManager(),"error_dialog");
+            dialog.show(getFragmentManager(), "error_dialog");
         }
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemp() + "");
+        mTimeLabel.setText("At " + mCurrentWeather.getFormattedTime() + " it will be");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "");
+        mSummaryLabel.setText(mCurrentWeather.getSummary());
+
+        Drawable drawable = ContextCompat.getDrawable(this, mCurrentWeather.getIconId());
+        mIconImageView.setImageDrawable(drawable);
+
+
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setMtime(currently.getLong("time"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setTemp(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
     }
 
     private boolean isNetWorkAvailable() {
